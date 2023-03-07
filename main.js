979cc528757cce5e76bbd8ec94044a220cb789f5
Captain Nick Lucifer* <naryal2580@gmail.com>
2023-03-07 14:30:35 +0545

    .

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+const updateDOM(event) => {
+  try {
+
+    await event.Execute(); //ws::1
+
+    document.querySelector('.x').innerHTML = 'git';
+  } catch {
+    console.info("Who knows?");
+  }
+}
+
 socket.on("D@3ie", (event) => {
-  console.log(event)
+  updateDOM()
 });
 
 socket.io.on("error", (error) => {
   console.error(error); // sorry?
 });
 
-// socket.offAny(); :|
+const G3nZ = async (iNu_, ...args) => {
+  console.info(iNu_);
+  obj = JSON(args);
+  try {
+    OQr_own_impl.soon.__and_bruh_ignore_prototype_Pollutions_over_enc_relays = await fetch(`https://qrcode.show/${obj}`) => {
+  resp => {
+    // update dom. // define [tim/MD/QR/emoji] class-es.
+    console.log(resp);
+    document.querySelector("#x").innerHTML = "Pv!";
+  } catch (err) {
+    console.info(err); // Failed to fetch
+  }
+}
+}
+}; // TS. - safe-guard-urOWNobjurSELF. - tq. ms. + git:// and etc./
+
+socket.offAny("pv-bots"); // :| #peace
